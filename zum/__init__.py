1c78bb8c1618e49d0ad2dc33c78b5bdc361dd71f
Daniel Leal <dlleal@uc.cl>
2021-02-18 02:14:37 -0300

    Add the tests init file and downgrade the version

--- zum/__init__.py
+++ zum/__init__.py
@@ -2,5 +2,5 @@
 Init file for the zum module.
 """
 
-version_info = (0, 1, 0)
+version_info = (0, 0, 1)
 __version__ = ".".join([str(x) for x in version_info])
--- zum/__main__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-from zum.executor import Executor
-
-if __name__ == "__main__":
-    executor = Executor()
-    executor.execute(sys.argv[1])
