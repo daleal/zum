e3e706c0d08e234aa704eb9a3b8b81a903b81f38
Daniel Leal <dlleal@uc.cl>
2021-03-20 03:14:11 -0300

    Move the execution logic to a dedicated module

---
+++ zum/executor.py
@@ -0,0 +1,13 @@
+"""
+A module to hold the request execution logic for the zum library.
+"""
+
+import httpx
+
+from zum.requests.models import Request
+
+
+def execute(base_url: str, request: Request) -> httpx.Response:
+    """Executes a request with the base URL."""
+    url = f"{base_url.rstrip('/')}{request.route}"
+    return httpx.request(request.method, url, json=request.body)
