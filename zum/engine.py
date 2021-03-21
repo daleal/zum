a52a3c486de959f89609ac1b62cc74bfafdcc3a8
Daniel Leal <37158127+daleal@users.noreply.github.com>
2021-03-21 20:45:12 -0300

    Add tests (#27)
    * Test parser generator
    
    * Remove empty test
    
    * Test validations
    
    * Change monkeypather to return a mock response type
    
    * Remove privacy features of the configurations validation
    
    * Add tests for almost all the behaviour
    
    * Finish testing the engine
    
    * Complete code coverage
    
    * Surrender to Pylinters

--- zum/engine.py
+++ zum/engine.py
@@ -25,13 +25,13 @@ class Engine:
     Class to handle the overall behaviour of zum.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, config_file_name: str = CONFIG_FILE_NAME) -> None:
         self.__output: Optional[str] = None
         self.__exception: Optional[Exception] = None
 
         try:
-            search_for_config_file(CONFIG_FILE_NAME)
-            configs = retrieve_config_file(CONFIG_FILE_NAME)
+            search_for_config_file(config_file_name)
+            configs = retrieve_config_file(config_file_name)
             validate_configs(configs)
         except (MissingConfigFileError, InvalidConfigFileError) as error:
             configs = {}
@@ -54,13 +54,13 @@ class Engine:
         """
         Executes the main zum logic.
         """
-        self.__validate_configurations()
+        self._validate_configurations()
         validate_raw_endpoint(self.__endpoints[instruction])
         request = generate_request(self.__endpoints[instruction], arguments)
         response = execute(self.__metadata["server"], request)
         self.__handle_response(response)
 
-    def __validate_configurations(self) -> None:
+    def _validate_configurations(self) -> None:
         """Validates that the configurations were correctly loaded."""
         if self.__exception:
             raise self.__exception
