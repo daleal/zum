15a0e5ca935e2021f825743190376c4fbb6f8dbb
Daniel Leal <dlleal@uc.cl>
2021-03-21 20:00:50 -0300

    Change monkeypather to return a mock response type

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,12 +9,30 @@ import pytest
 
 @pytest.fixture
 def patch_request(monkeypatch):
+    class Response:
+        def __init__(self, method, url, *args, **kwargs):
+            self.method = method
+            self.url = url
+            self.args = args
+            self.kwargs = kwargs
+            self._error = TypeError if method is None else None
+            self._error_text = "No method specified"
+
+        @property
+        def text(self):
+            return self._error_text
+
+        def json(self):
+            if self._error:
+                raise self._error(self._error_text)
+            return {
+                "method": self.method,
+                "url": self.url,
+                **self.kwargs,
+            }
+
     def mock_request(method, url, *args, **kwargs):
-        return {
-            "method": method,
-            "url": url,
-            "headers": kwargs["headers"],
-            "json": kwargs["json"],
-        }
+        return Response(method, url, *args, **kwargs)
 
     monkeypatch.setattr(httpx, "request", mock_request)
+    monkeypatch.setattr(httpx, "Response", Response)
