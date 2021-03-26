8b41f2fcbba893f9243978831029e8056ef5afd9
Daniel Leal <dlleal@uc.cl>
2021-03-26 00:53:09 -0300

    Add tests for passing the filename through the CLI

--- tests/cli/test_core.py
+++ tests/cli/test_core.py
@@ -9,12 +9,15 @@ from zum.constants import DEFAULT_CONFIG_FILE_NAME
 
 class TestDispatcher:
     def setup_method(self):
-        self.invalid_config = (
+        self.invalid_config_file_name = "invalid.toml"
+        self.valid_config_file_name = "valid.toml"
+        self.endpoint_name = "exmple"
+        self.configs = (
             "[metadata]\n"
-            "server = ''\n"
-            "[endpoints.test]\n"
-            "route = '/test'\n"
-            "method = 'get'\n"
+            'server = "http://localhost:8000"\n'
+            f"[endpoints.{self.endpoint_name}]\n"
+            'route = "/example"\n'
+            'method = "get"\n'
         )
 
     def test_help_flag(self, capsys):
@@ -28,6 +31,31 @@ class TestDispatcher:
             dispatcher(["--version"])
         captured = capsys.readouterr().out
         assert f"zum version {zum.__version__}" in captured
+
+    def test_invalid_config_file(self, tmpdir, capsys):
+        invalid_config_file = tmpdir.join(self.invalid_config_file_name)
+        valid_config_file = tmpdir.join(self.valid_config_file_name)
+        with open(valid_config_file.strpath, "w") as raw_config_file:
+            raw_config_file.write(self.configs)
+
+        with pytest.raises(SystemExit):
+            dispatcher(["--file", invalid_config_file.strpath, "--help"])
+
+        captured = capsys.readouterr().out
+        assert "No config file" in captured
+        assert self.invalid_config_file_name in captured
+
+    def test_valid_config_file(self, tmpdir, capsys):
+        valid_config_file = tmpdir.join(self.valid_config_file_name)
+        with open(valid_config_file.strpath, "w") as raw_config_file:
+            raw_config_file.write(self.configs)
+
+        with pytest.raises(SystemExit):
+            dispatcher(["--file", valid_config_file.strpath, "--help"])
+
+        captured = capsys.readouterr().out
+        assert "No config file" not in captured
+        assert f"{{{self.endpoint_name}}}" in captured
 
 
 class TestGetConfigFileName:
